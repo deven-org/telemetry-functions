2e57772626c44993b8a2cca9ab2cd3c606ec1470
Raffaele Pizzari <raffaele.pizzari@accenture.com>
2023-03-20 14:46:47 +0100

    fix(tests): tooling usage

--- src/core/storeData.ts
+++ src/core/storeData.ts
@@ -6,15 +6,34 @@ import octokit from "./octokit";
 export const storeData = async (
   enhancedDataEvents: Promise<EnhancedDataEvent>[]
 ) => {
+  console.log("Entering store data...");
+  console.log("enhancedDataEvents", enhancedDataEvents);
   if (!enhancedDataEvents) return false;
   const resolvedEnhancedDataEvents = await Promise.all(enhancedDataEvents);
 
+  console.log("resolvedEnhancedDataEvents", resolvedEnhancedDataEvents);
   resolvedEnhancedDataEvents.forEach(async (data) => {
     logger.info(
       `Pushing file to repo: ${process.env.REPO_PATH}/${data.created_at}.json`
     );
 
     try {
+      console.log({
+        owner: process.env.REPO_OWNER as string,
+        repo: process.env.REPO_NAME as string,
+        path: `${process.env.REPO_PATH}/${data.created_at}.json`,
+        message: `auto(data): add ${data.dataEventSignature} for ${data.owner}/${data.repo}`,
+        committer: {
+          name: process.env.COMMITTER_NAME as string,
+          email: process.env.COMMITTER_EMAIL as string,
+        },
+        author: {
+          name: process.env.AUTHOR_NAME as string,
+          email: process.env.AUTHOR_EMAIL as string,
+        },
+        content: Base64.encode(JSON.stringify(data)),
+      });
+
       await octokit.request("PUT /repos/{owner}/{repo}/contents/{path}", {
         owner: process.env.REPO_OWNER as string,
         repo: process.env.REPO_NAME as string,
@@ -31,8 +50,10 @@ export const storeData = async (
         content: Base64.encode(JSON.stringify(data)),
       });
     } catch (e) {
+      console.log("catch");
       console.log(e);
     }
+    console.log("after request");
     logger.complete(
       `File pushed to repo: ${process.env.REPO_PATH}/${data.created_at}.json`
     );
