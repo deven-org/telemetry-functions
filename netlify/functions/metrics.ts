412df535b8df3f6cbe2a549e74923b5cc91dd71f
Raffaele Pizzari <raffaele.pizzari@accenture.com>
2023-03-20 14:48:58 +0100

    fix(tests): tooling usage

--- netlify/functions/metrics.ts
+++ netlify/functions/metrics.ts
@@ -7,12 +7,14 @@ const handler: Handler = async (event: HandlerEvent) => {
   try {
     const eventSignature = event.headers["x-github-event"] || "unknown";
     const body = getEventBody(event);
-    await collectMetricsHandler({ eventSignature, ...body });
+    const metrics = await collectMetricsHandler({ eventSignature, ...body });
+    console.log(metrics);
     return {
       statusCode: 200,
       body: "success",
     };
   } catch (e: any) {
+    console.log(e);
     return {
       statusCode: 500,
       body: e && e.message ? e.message : e,
