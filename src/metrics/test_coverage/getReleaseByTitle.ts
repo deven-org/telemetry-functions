b7777676c499e0ec33cdfade2840bf6a5cd728b3
Raffaele Pizzari <raffaele.pizzari@accenture.com>
2023-03-23 14:54:36 +0100

    feat(test-coverage): add tests metrics

--- src/metrics/test_coverage/getReleaseByTitle.ts
+++
@@ -1,9 +0,0 @@
-import { pick } from "ramda";
-import semverCoerce from "semver/functions/coerce";
-
-export const getReleaseByTitle = (title: string) =>
-  pick(
-    ["raw", "major", "minor", "patch", "prerelease", "build", "version"],
-    semverCoerce(title)
-  );
- 
