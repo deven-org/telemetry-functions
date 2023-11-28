aa67028b286511732ecb4e23ec5499ca0efa6fb9
Remmy Cat Stock <remmy.cat.stock@accenture.com>
2023-11-28 16:49:40 +0100

    fix: better representation of commit timestamps - don't default to now

--- src/metrics/commits_per_pr/interfaces.d.ts
+++ src/metrics/commits_per_pr/interfaces.d.ts
@@ -6,6 +6,12 @@ export interface CommitsPerPrOutput {
   commits: number;
   additions: number;
   deletions: number;
-  commit_timestamps: [];
+  commit_timestamps: {
+    /** Time commit was authored (UNIX ms) */
+    authored: number | null;
+
+    /** Time commit was committed (UNIX ms) */
+    committed: number | null;
+  }[];
   pr_id: number;
 }
