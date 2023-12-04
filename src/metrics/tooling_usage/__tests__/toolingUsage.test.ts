1ae633711f3e2787d488aacd714bdfece38bf358
Remmy Cat Stock <remmy.cat.stock@accenture.com>
2023-12-04 11:13:42 +0100

    feat: improve api-dependent tooling_usage output

--- src/metrics/tooling_usage/__tests__/toolingUsage.test.ts
+++ src/metrics/tooling_usage/__tests__/toolingUsage.test.ts
@@ -1,20 +1,22 @@
-import { DataEventSignature, MetricsSignature } from "../../../interfaces";
+import {
+  DataEventSignature,
+  MetricsSignature,
+  RawEvent,
+} from "../../../interfaces";
 import { handler } from "../../../handler";
 import { encode } from "js-base64";
+import { Mocktokit } from "../../../__tests__/mocktokit";
+import { ToolingUsagePayload } from "../interfaces";
 
 // Only collect this metric
 jest.mock("../../../metricsConditions.ts", () =>
   jest.requireActual("../metricsConditions")
 );
 
-let octokitResponse = {};
-
-jest.mock("./../../../core/octokit.ts", () => ({
-  __esModule: true,
-  default: {
-    request: () => octokitResponse,
-  },
-}));
+jest.mock(
+  "./../../../core/octokit.ts",
+  () => jest.requireActual("../../../__tests__/mocktokit").octokitModuleMock
+);
 
 jest.mock("../../../core/logger.ts", () => ({
   __esModule: true,
@@ -32,100 +34,183 @@ jest.mock("../../../core/logger.ts", () => ({
 }));
 
 describe("Tooling_Usage", () => {
-  it("event gets signed as a toolingUsage event", async () => {
-    const eventBody = {
-      eventSignature: "toolingUsage",
-    };
+  const FAKE_NOW = 1700000000000;
 
-    octokitResponse = {
+  const GET_CONFIG_ENDPOINT = "GET /repos/{owner}/{repo}/contents/{path}";
+
+  const eventBody: RawEvent & ToolingUsagePayload = {
+    eventSignature: "toolingUsage",
+    owner: "test-owner",
+    repo: "test-repo",
+  };
+
+  beforeAll(() => {
+    jest.useFakeTimers({ now: FAKE_NOW });
+  });
+
+  beforeEach(() => {
+    Mocktokit.reset({
+      // endpoint to save json data
+      ["PUT /repos/{owner}/{repo}/contents/{path}"]: async () => undefined,
+    });
+  });
+
+  afterEach(() => {
+    expect(Mocktokit.unexpectedRequestsMade).toStrictEqual([]);
+  });
+
+  it("event gets signed as a toolingUsage event", async () => {
+    Mocktokit.mocks[GET_CONFIG_ENDPOINT] = async () => ({
       data: {
         content: encode(JSON.stringify({})),
       },
-    };
+    });
 
     const result: [] = await handler(eventBody);
 
     expect(result).toMatchObject([
       {
-        created_at: expect.any(Number),
+        created_at: FAKE_NOW,
         output: {},
         dataEventSignature: DataEventSignature.ToolingUsage,
+        owner: "test-owner",
+        repo: "test-repo",
       },
     ]);
   });
 
-  it("returns true if deven-documentation-skeleton config file is available", async () => {
-    const eventBody = {
-      eventSignature: "toolingUsage",
-    };
-
-    octokitResponse = {
-      data: {
-        content: encode(JSON.stringify({})),
-      },
+  it("sets exists: false, if config file is missing", async () => {
+    Mocktokit.mocks[GET_CONFIG_ENDPOINT] = async () => {
+      const error: Error & { status?: number } = new Error("test: not found");
+      error.status = 404;
+      throw error;
     };
 
     const result: [] = await handler(eventBody);
 
-    expect(result).toMatchObject([
+    expect(result).toStrictEqual([
       {
-        created_at: expect.any(Number),
+        created_at: FAKE_NOW,
+        status: "success",
         output: {
-          hasDocumentationSkeleton: true,
-          documentationSkeletonVersion: undefined,
+          documentationSkeletonConfig: {
+            exists: false,
+            parsable: null,
+            version: null,
+          },
         },
         dataEventSignature: DataEventSignature.ToolingUsage,
         metricsSignature: MetricsSignature.ToolingUsage,
+        owner: "test-owner",
+        repo: "test-repo",
+      },
+    ]);
+  });
+
+  it("sets exists: true, if config file is available", async () => {
+    Mocktokit.mocks[GET_CONFIG_ENDPOINT] = async () => ({
+      data: {
+        content: encode(JSON.stringify([1, 2, 3])),
+      },
+    });
+
+    const result: [] = await handler(eventBody);
+
+    expect(result).toStrictEqual([
+      {
+        created_at: FAKE_NOW,
+        status: "success",
+        output: {
+          documentationSkeletonConfig: {
+            exists: true,
+            parsable: false,
+            version: null,
+          },
+        },
+        dataEventSignature: DataEventSignature.ToolingUsage,
+        metricsSignature: MetricsSignature.ToolingUsage,
+        owner: "test-owner",
+        repo: "test-repo",
+      },
+    ]);
+  });
+
+  it("sets parsable: true, if config file is parsable", async () => {
+    Mocktokit.mocks[GET_CONFIG_ENDPOINT] = async () => ({
+      data: {
+        content: encode(JSON.stringify({ some: "data" })),
+      },
+    });
+
+    const result: [] = await handler(eventBody);
+
+    expect(result).toStrictEqual([
+      {
+        created_at: FAKE_NOW,
+        status: "success",
+        output: {
+          documentationSkeletonConfig: {
+            exists: true,
+            parsable: true,
+            version: null,
+          },
+        },
+        dataEventSignature: DataEventSignature.ToolingUsage,
+        metricsSignature: MetricsSignature.ToolingUsage,
+        owner: "test-owner",
+        repo: "test-repo",
       },
     ]);
   });
 
   it("returns version if available in deven-documentation-skeleton config file", async () => {
-    const eventBody = {
-      eventSignature: "toolingUsage",
-    };
-
-    octokitResponse = {
+    Mocktokit.mocks[GET_CONFIG_ENDPOINT] = async () => ({
       data: {
         content: encode(JSON.stringify({ version: "2.0.0" })),
       },
-    };
+    });
 
     const result: [] = await handler(eventBody);
 
-    expect(result).toMatchObject([
+    expect(result).toStrictEqual([
       {
-        created_at: expect.any(Number),
+        created_at: FAKE_NOW,
+        status: "success",
         output: {
-          hasDocumentationSkeleton: true,
-          documentationSkeletonVersion: "2.0.0",
+          documentationSkeletonConfig: {
+            exists: true,
+            parsable: true,
+            version: "2.0.0",
+          },
         },
         dataEventSignature: DataEventSignature.ToolingUsage,
         metricsSignature: MetricsSignature.ToolingUsage,
+        owner: "test-owner",
+        repo: "test-repo",
       },
     ]);
   });
 
-  it("returns false if deven-documentation-skeleton config file has not been found", async () => {
-    const eventBody = {
-      eventSignature: "toolingUsage",
-    };
-
-    octokitResponse = {
-      status: 404,
+  it("sets status to networkError and config to null if config fetch fails", async () => {
+    Mocktokit.mocks[GET_CONFIG_ENDPOINT] = async () => {
+      const error: Error & { status?: number } = new Error("test: not found");
+      error.status = 500;
+      throw error;
     };
 
     const result = await handler(eventBody);
 
-    expect(result).toMatchObject([
+    expect(result).toStrictEqual([
       {
-        created_at: expect.any(Number),
+        created_at: FAKE_NOW,
+        status: "networkError",
         output: {
-          hasDocumentationSkeleton: false,
-          documentationSkeletonVersion: undefined,
+          documentationSkeletonConfig: null,
         },
         dataEventSignature: DataEventSignature.ToolingUsage,
         metricsSignature: MetricsSignature.ToolingUsage,
+        owner: "test-owner",
+        repo: "test-repo",
       },
     ]);
   });
