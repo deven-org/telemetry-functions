c17a2fbb5985000d1a9f9f13adf660e912607d5b
Remmy Cat Stock <remmy.cat.stock@accenture.com>
2023-12-05 16:59:39 +0100

    test: turn on additional linting rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,12 +5,13 @@ module.exports = {
     jest: true,
   },
   parser: "@typescript-eslint/parser",
-  extends: ["eslint:recommended" ,"plugin:@typescript-eslint/recommended"],
+  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
   parserOptions: {
     ecmaVersion: 2020,
   },
   rules: {
-    "@typescript-eslint/camelcase": 0,
-    "@typescript-eslint/no-explicit-any": 1,
+    "@typescript-eslint/camelcase": "off",
+    "@typescript-eslint/no-explicit-any": "error",
+    "default-case": "error",
   },
 };
